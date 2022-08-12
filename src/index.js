5cd492e37be2cc6f6f05909c3bb3eebbc0ba9424
Uliana <kchopin1@gmail.com>
2022-08-12 22:33:36 +0200

    add some changes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@
 
 module.exports = function towelSort(matrix) {
   
-  let array = [];
-    
-  for (let n in matrix) {
-      if (n%2 == 1) {
-        array.push(matrix[n].reverse().join('').split(''));
+let array = [];
 
-}
-
-      else array.push(matrix[n].join('').split(''));
+  for (i in matrix)
+  {
+    if (i % 2 == 0)
+    {
+      for (n in matrix[i])
+      {
+        array.push(matrix[i][n]);
     }
+  }
+    else
+      {
+        let a = matrix[i].reverse();
+        for (let n = 0; n < matrix[i].length; n++) {
+          array.push(matrix[i][n]);
+      }
+    }
+}
 
 return array;
   
